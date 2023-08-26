554b30795c4afe820e90ae74e9e9d48f1a9da577
Ehfaz Mahee <mahee504@gmail.com>
2023-08-26 12:47:17 +0600

    added home page,routes file,navbar,footer,Main layout and few minor changes

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <>
+            <h2>this is home</h2>
+        </>
+    );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,6 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake","garden"],
+    themes: ["light", "dark", "garden"],
   },
 };
