a3d7ffc90a7b15b812ab9646ab135fafacc0a360
Ehfaz Mahee <mahee504@gmail.com>
2023-08-27 00:32:26 +0600

    added and styled section title,edited category

---
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,15 @@
+const SectionTitle = ({ heading, subHeading }) => {
+  return (
+    <>
+      <div className=" mx-auto  md:w-4/12 my-8">
+        <p className="text-yellow-600  mb-2 text-center ">---{heading}---</p>
+        <h1 className="text-3xl text-center uppercase border-slate-400 border-y-4 py-4">
+          {" "}
+          {subHeading}
+        </h1>{" "}
+      </div>
+    </>
+  );
+};
+
+export default SectionTitle;
--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -5,109 +5,118 @@ import "swiper/css/pagination";
 
 // import required modules
 import { Pagination } from "swiper/modules";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
     <>
-      <Swiper
-        slidesPerView={4}
-        spaceBetween={30}
-        centeredSlides={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide className="mb-[100px]">
-          <div className="relative">
-            <img src="https://i.ibb.co/v4QTBMT/slide1.jpg" alt="" />
-            <div className="absolute rounded-sm  top-0 left-0  p-2 bg-black opacity-40 ">
-              <h1 className="text-white">0</h1>
-            </div>
-            <h1 className="text-4xl text-center -mt-20 -ml-6 text-white ">
-              Fruit Salad
-            </h1>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative">
-            <img src="https://i.ibb.co/xsm0hnB/slide2.jpg" alt="" />
-            <div className="absolute rounded-lg  top-0 left-0    p-2 bg-black opacity-40 ">
-              <span className="text-white">1</span>
-            </div>
-            <h1 className="text-4xl text-center -mt-20 -ml-6 text-white ">
-              Fruit Salad
-            </h1>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative">
-            <img src="https://i.ibb.co/xmSvD7M/slide3.jpg" alt="" />
-            <div className="absolute rounded-lg  top-0 left-0    p-2 bg-black opacity-40 ">
-              <span className="text-white">2</span>
-            </div>
-            <h1 className="text-4xl text-center -mt-20 -ml-6 text-white ">
-              Fruit Salad
-            </h1>
-          </div>
-        </SwiperSlide>
+      <section>
+        <SectionTitle
+        heading={"From11.00 am to 11.0 pm"}
+        subHeading={"Order Online"}
+        >
 
-        <SwiperSlide>
-          <div className="relative">
-            <img src="https://i.ibb.co/Rg70b4G/slide4.jpg" alt="" />
-            <div className="absolute rounded-lg  top-0 left-0    p-2 bg-black opacity-40 ">
-              <span className="text-white">3</span>
+        </SectionTitle>
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={30}
+          centeredSlides={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide className="mb-[100px]">
+            <div className="relative">
+              <img src="https://i.ibb.co/v4QTBMT/slide1.jpg" alt="" />
+              <div className="absolute rounded-sm  top-0 left-0  p-2 bg-black opacity-40 ">
+                <h1 className="text-white">0</h1>
+              </div>
+              <h1 className="text-4xl text-center -mt-20 -ml-6 text-white ">
+                Fruit Salad
+              </h1>
             </div>
-            <h1 className="text-4xl text-center -mt-20 -ml-6 text-white ">
-              Fruit Salad
-            </h1>
-          </div>
-        </SwiperSlide>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative">
+              <img src="https://i.ibb.co/xsm0hnB/slide2.jpg" alt="" />
+              <div className="absolute rounded-lg  top-0 left-0    p-2 bg-black opacity-40 ">
+                <span className="text-white">1</span>
+              </div>
+              <h1 className="text-4xl text-center -mt-20 -ml-6 text-white ">
+                Fruit Salad
+              </h1>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative">
+              <img src="https://i.ibb.co/xmSvD7M/slide3.jpg" alt="" />
+              <div className="absolute rounded-lg  top-0 left-0    p-2 bg-black opacity-40 ">
+                <span className="text-white">2</span>
+              </div>
+              <h1 className="text-4xl text-center -mt-20 -ml-6 text-white ">
+                Fruit Salad
+              </h1>
+            </div>
+          </SwiperSlide>
 
-        <SwiperSlide>
-          <div className="relative">
-            <img
-              src="https://i.ibb.co/5hDy04x/pexels-dapur-melodi-1109197.jpg"
-              alt=""
-            />
-            <div className="absolute rounded-lg  top-0 left-0    p-2 bg-black opacity-40 ">
-              <span className="text-white">4</span>
+          <SwiperSlide>
+            <div className="relative">
+              <img src="https://i.ibb.co/Rg70b4G/slide4.jpg" alt="" />
+              <div className="absolute rounded-lg  top-0 left-0    p-2 bg-black opacity-40 ">
+                <span className="text-white">3</span>
+              </div>
+              <h1 className="text-4xl text-center -mt-20 -ml-6 text-white ">
+                Fruit Salad
+              </h1>
             </div>
-            <h1 className="text-4xl text-center -mt-20 -ml-6 text-white ">
-              Fruit Salad
-            </h1>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative">
-            <img
-              src="https://i.ibb.co/tPT2JZC/pexels-william-choquette-2641886.jpg"
-              alt=""
-            />
-            <div className="absolute rounded-lg  top-0 left-0    p-2 bg-black opacity-40 ">
-              <span className="text-white">5</span>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="relative">
+              <img
+                src="https://i.ibb.co/5hDy04x/pexels-dapur-melodi-1109197.jpg"
+                alt=""
+              />
+              <div className="absolute rounded-lg  top-0 left-0    p-2 bg-black opacity-40 ">
+                <span className="text-white">4</span>
+              </div>
+              <h1 className="text-4xl text-center -mt-20 -ml-6 text-white ">
+                Fruit Salad
+              </h1>
             </div>
-            <h1 className="text-4xl text-center -mt-20 -ml-6 text-white ">
-              Fruit Salad
-            </h1>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative">
-            <img
-              src="https://i.ibb.co/2gzmW6Y/pexels-valeria-boltneva-842571.jpg"
-              alt=""
-            />
-            <div className="absolute rounded-lg  top-0 left-0   p-2 bg-black opacity-40 ">
-              <span className="text-white">6</span>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative">
+              <img
+                src="https://i.ibb.co/tPT2JZC/pexels-william-choquette-2641886.jpg"
+                alt=""
+              />
+              <div className="absolute rounded-lg  top-0 left-0    p-2 bg-black opacity-40 ">
+                <span className="text-white">5</span>
+              </div>
+              <h1 className="text-4xl text-center -mt-20 -ml-6 text-white ">
+                Fruit Salad
+              </h1>
             </div>
-            <h1 className="text-4xl text-center -mt-20 -ml-6 text-white ">
-              Fruit Salad
-            </h1>
-          </div>
-        </SwiperSlide>
-      </Swiper>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative">
+              <img
+                src="https://i.ibb.co/2gzmW6Y/pexels-valeria-boltneva-842571.jpg"
+                alt=""
+              />
+              <div className="absolute rounded-lg  top-0 left-0   p-2 bg-black opacity-40 ">
+                <span className="text-white">6</span>
+              </div>
+              <h1 className="text-4xl text-center -mt-20 -ml-6 text-white ">
+                Fruit Salad
+              </h1>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </section>
     </>
   );
 };
