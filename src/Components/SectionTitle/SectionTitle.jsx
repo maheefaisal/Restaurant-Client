d243360d99538ee0e608ef9ffa70efe24707e885
Ehfaz Mahee <mahee504@gmail.com>
2023-08-27 02:18:45 +0600

    added popular menu ,menu item,few minor changes

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line react/prop-types
 const SectionTitle = ({ heading, subHeading }) => {
   return (
     <>
---
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,19 @@
+const MenuItem = ({ item }) => {
+  const { image, price, name, recipe } = item;
+  return (
+    <>
+    <div className="flex space-x-2">
+        
+      <img style={{borderRadius:'0 200px 200px 200px'}} className="w-[120px]" src={image} alt="" />
+      <div>
+        <p className="uppercase">Name: {name}___________________</p>
+        <p>Recepie:{recipe}</p>
+      </div>
+      <p className="text-yellow-500">Price: {price}</p>
+      
+    </div>
+    </>
+  );
+};
+
+export default MenuItem;
