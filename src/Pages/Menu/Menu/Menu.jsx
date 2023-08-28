c747cc54cb780917f7e5f935b7f16fdfe2cf2483
Ehfaz Mahee <mahee504@gmail.com>
2023-08-28 18:33:14 +0600

    added custo hooks,menucategory

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,8 +1,7 @@
 
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
-import BreadCrumbs from '../../Shared/BreadCrumbs/BreadCrumbs';
+
 
 const Menu = () => {
     return (
@@ -15,22 +14,6 @@ const Menu = () => {
                 title={'our menu'}
                 subTitle={'Would you like to try a dish?'}
             ></Cover>
-    
-            <PopularMenu></PopularMenu>
-            <Cover
-                img={'https://i.ibb.co/ftWXPmq/banner3.jpg'}
-                title={'our menu'}
-                subTitle={'Would you like to try a dish?'}
-            ></Cover>
-    
-            <PopularMenu></PopularMenu>
-            <Cover
-                img={'https://i.ibb.co/ftWXPmq/banner3.jpg'}
-                title={'our menu'}
-                subTitle={'Would you like to try a dish?'}
-            ></Cover>
-    
-            <PopularMenu></PopularMenu>
         </>
     );
 };
