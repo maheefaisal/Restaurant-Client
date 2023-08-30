72379341edaa3ffc40291350c77450522abdd7c6
Ehfaz Mahee <mahee504@gmail.com>
2023-08-30 19:08:16 +0600

    added contact form and few minor changes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Menu from "../Pages/Menu/Menu/Menu";
 import Order from "../Pages/Order/Order/Order";
+import ContactUs from "../Pages/ContactUs/ContactUs";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -19,6 +20,10 @@ export const router = createBrowserRouter([
       {
         path: "/order/:category",
         element: <Order />
+      },
+      {
+        path: "/contactus",
+        element: <ContactUs />
       },
     ],
   },
