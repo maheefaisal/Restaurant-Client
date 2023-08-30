00ec2c45cee6a43a8b21ab4f364b6639e7888d5c
Ehfaz Mahee <mahee504@gmail.com>
2023-08-30 22:38:07 +0600

    added login page

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -16,6 +16,9 @@ const Navbar = () => {
       <li>
         <Link to={"/contactus"}>ContactUs</Link>
       </li>
+      <li>
+        <Link to={"/login"}>Login</Link>
+      </li>
     </>
   );
   return (
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "../Pages/Home/Home/Home";
 import Menu from "../Pages/Menu/Menu/Menu";
 import Order from "../Pages/Order/Order/Order";
 import ContactUs from "../Pages/ContactUs/ContactUs";
+import Login from "../Pages/Login/Login";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -24,6 +25,10 @@ export const router = createBrowserRouter([
       {
         path: "/contactus",
         element: <ContactUs />
+      },
+      {
+        path: "/login",
+        element: <Login />
       },
     ],
   },
