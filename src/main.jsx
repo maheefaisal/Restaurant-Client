c5b4f54d988041b531f33eddcb28985d2fd5f25d
Ehfaz Mahee <mahee504@gmail.com>
2023-09-04 21:06:47 +0600

    added package tanstack,added few changes to usecart and main

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,27 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes";
 import { HelmetProvider } from 'react-helmet-async';
 import Authprovider from "./Providers/Authprovider";
+import {
+
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Authprovider>
 
       <HelmetProvider>
-        <div className="max-w-screen-xl mx-auto">
-          <RouterProvider router={router} />
-        </div>
+
+        <QueryClientProvider client={queryClient}>
+
+          <div className="max-w-screen-xl mx-auto">
+            <RouterProvider router={router} />
+          </div>
+        </QueryClientProvider>
+
 
       </HelmetProvider>
     </Authprovider>
