2152b7ea24ba2c03059ebd55f8c5c81d9254aa91
Ehfaz Mahee <mahee504@gmail.com>
2023-09-12 22:48:53 +0600

    added token

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -5,11 +5,15 @@ import { AuthContext } from '../Providers/Authprovider';
 const useCart = () => {
     const { user } = useContext(AuthContext)
 
-
-    const { refetch,data: cart = [] } = useQuery({
+    const token = localStorage.getItem('access_token')
+    const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {
+                headers: {
+                    authorization: `Bearer ${token}`
+                }
+            })
             return response.json()
         },
     })
