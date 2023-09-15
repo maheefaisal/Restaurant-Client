e65c31ebc572712b1b7c8afe4f6ab3159100f8aa
Ehfaz Mahee <mahee504@gmail.com>
2023-09-15 17:14:50 +0600

    added axios secure

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,19 +1,19 @@
 import { useQuery } from '@tanstack/react-query'
 import { useContext } from 'react';
 import { AuthContext } from '../Providers/Authprovider';
+import useAxiosSecure from './useAxiosSecure';
 
 const useCart = () => {
     const { user } = useContext(AuthContext)
 
-    const token = localStorage.getItem('access_token')
+    // const token = localStorage.getItem('access_token')
+    const [axiosSecure] = useAxiosSecure();
+
     const { refetch, data: cart = [] } = useQuery({
+
         queryKey: ['carts', user?.email],
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {
-                headers: {
-                    authorization: `Bearer ${token}`
-                }
-            })
+            const response = await axiosSecure(`/carts?email=${user?.email}`)
             return response.json()
         },
     })
