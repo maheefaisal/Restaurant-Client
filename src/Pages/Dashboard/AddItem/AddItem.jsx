00afb6f6f797f0751a96cb47943e00bf44ea93ad
Ehfaz Mahee <mahee504@gmail.com>
2023-09-25 04:30:47 +0600

    changed add items

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -1,11 +1,12 @@
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { useForm } from 'react-hook-form';
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const image_hosting_token = import.meta.env.VITE_Image_Upload_Token
 const AddItem = () => {
-
-
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [axiosSecure] = useAxiosSecure()
+    const { register, handleSubmit, reset } = useForm();
     const img_hosting_url = `https://api.imgbb.com/1/upload?key=${image_hosting_token}`
     const onSubmit = data => {
         const formData = new FormData()
@@ -17,10 +18,24 @@ const AddItem = () => {
             .then(res => res.json())
             .then(imgResponse => {
                 if (imgResponse.success) {
+                    reset()
                     const imgURL = imgResponse.data.display_url
                     const { name, price, category, recipe } = data
-                    const newItem = { name, price:parseFloat(price), category, recipe ,image:imgURL}
+                    const newItem = { name, price: parseFloat(price), category, recipe, image: imgURL }
                     console.log(newItem);
+                    axiosSecure.post('/menu', newItem)
+                        .then(data => {
+                            console.log('After posting new menu item', data.data)
+                            if (data.data.insertedId) {
+                                Swal.fire({
+                                    position: 'top-end',
+                                    icon: 'success',
+                                    title: 'Item Added Successfully',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                })
+                            }
+                        })
                 }
             })
     };
