5352a31efef931506995bb891d6233e581e94d5c
Ehfaz Mahee <mahee504@gmail.com>
2023-09-27 20:06:56 +0600

    changes in usemenu,manageitem

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -15,7 +15,7 @@ const useMenu = () => {
 
     // });
 
-    const { data:menu = [], isLoading:loading} = useQuery({
+    const { data: menu = [], isLoading: loading, refetch } = useQuery({
         queryKey: ['menu'],
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/menu')
@@ -23,7 +23,7 @@ const useMenu = () => {
         }
     })
 
-    return [menu, loading]
+    return [menu, loading, refetch]
 
 }
 
