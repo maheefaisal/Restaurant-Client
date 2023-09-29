4eed9407de99f7e8cbc0b7ae822b96f68d15a9d0
Ehfaz Mahee <mahee504@gmail.com>
2023-09-29 23:35:45 +0600

    made changes to payment

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,11 +1,18 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
 
-
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateaway_PK)
 const Payment = () => {
     return (
         <div>
             <SectionTitle subHeading={"Please Provide"} heading={"Payment"} ></SectionTitle>
             <h2 className="text-3xl">Money Ohhhhh Money</h2>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm />
+
+            </Elements>
         </div>
     );
 };
